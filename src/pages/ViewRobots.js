fcb53b9b85ad9865f0c6e13a18905f68637a1af0
Fowad Sohail <fowad.sohail@gmail.com>
2022-04-07 16:52:38 -0400

    view-single-robot

--- src/pages/ViewRobots.js
+++ src/pages/ViewRobots.js
@@ -1,9 +1,26 @@
-import { Card } from '@mui/material';
+import { Divider, CardMedia, Card, CardHeader, Grid, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
 
-export default function ViewRobots() {
+export default function ViewRobots({ robots }) {
   return (
-    <Card sx={{ marginTop: 10 }}>
-      <h2>ViewRobots</h2>
-    </Card>
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Grid container spacing={3} sx={{ marginTop: 10, maxWidth: '80%' }}>
+        {robots?.map((robot) => (
+          <Grid item md={6}>
+            <Link to='/robot' state={{ robot: robot }}>
+              <Card elevation={3}>
+                <CardHeader title={robot?.name} />
+                <Divider />
+                <CardMedia
+                  component='img'
+                  image='/robot-downey-jr.png'
+                  alt='Robot Downey Jr.'
+                />
+              </Card>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
--- src/pages/ViewSingleRobot.js
+++ src/pages/ViewSingleRobot.js
@@ -1,9 +1,77 @@
-import { Card } from '@mui/material';
+import {
+  Button,
+  Grid,
+  TextField,
+  Box,
+  Typography,
+  Divider,
+} from '@mui/material';
+import { useLocation, Link } from 'react-router-dom';
 
 export default function ViewSingleRobot() {
+  const location = useLocation();
+  const { robot } = location.state;
+
   return (
-    <Card sx={{ marginTop: 10 }}>
-      <h2>View Single Robot</h2>
-    </Card>
+    <Box
+      sx={{
+        marginTop: 10,
+        maxWidth: '80%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <Typography variant='h4'>View Details for {robot?.name}</Typography>
+      <Divider />
+      <Grid container spacing={3} sx={{ marginTop: 10 }}>
+        <Grid item md={6}>
+          <img alt='Robot Downey Jr.' width='360' src='/robot-downey-jr.png' />
+        </Grid>
+        <Grid
+          item
+          md={6}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <TextField
+            disabled
+            label='Name'
+            variant='outlined'
+            defaultValue={robot?.name}
+          />
+          <TextField
+            sx={{ marginTop: 3 }}
+            disabled
+            label='Color'
+            variant='outlined'
+            defaultValue={robot?.color}
+          />
+          <TextField
+            sx={{ marginTop: 3 }}
+            disabled
+            label='Attack'
+            variant='outlined'
+            defaultValue={robot?.attack}
+          />
+          <TextField
+            sx={{ marginTop: 3 }}
+            disabled
+            label='Defense'
+            variant='outlined'
+            defaultValue={robot?.defense}
+          />
+        </Grid>
+      </Grid>
+      <Box sx={{ marginTop: 3, display: "flex", flexDirection: 'row-reverse' }}>
+        <Button>Delete</Button>
+        <Link to='/app'>
+        <Button>Back</Button>
+        </Link>
+      </Box>
+    </Box>
   );
 }
